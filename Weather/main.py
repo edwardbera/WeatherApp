1b86c0a680a0ad21e3060571dbfb35a16ee176ba
edwardbera <beraedward@gmail.com>
2023-01-21 22:38:05 +0200

    modified:   Weather/main.py 	modified:   Weather/weather.json 	deleted:    Weather/whatsapp.py 	deleted:    weather.json 	deleted:    webhook.py 	deleted:    whatsapp.py

--- Weather/main.py
+++ Weather/main.py
@@ -1,21 +1,8 @@
-from textwrap import indent
-from webbrowser import get
-from xml.dom.minidom import Element
 import requests
-from bs4 import BeautifulSoup
 import json
 import tkinter
-from whatsapp import *
 from tkinter import *
 
-
-city = "Bulawayo"
-
-clouds ="1"
-humidity=""
-max = "" 
-min = ""
-wind = " "
 
 def getWeather(location):
     
@@ -28,7 +15,6 @@ def getWeather(location):
         json.dump(data, f, indent=2)
         f.close()
 
-        #print(results.json())
 
 def readJson():
     j = open('weather.json', "r")
@@ -47,15 +33,12 @@ def readJson():
     city = c
 
     weather = []
-
     weather.append(sky)
     weather.append(humidity)
     weather.append(max_temp)
     weather.append(min_temp)
     weather.append(city)
     weather.append(wind)
-
-    #print( )
     return weather
 
 
@@ -70,64 +53,48 @@ def update(cl, hm, mx, mn, location, wind):
     
 def getInput():
     inp = input.get(1.0, END)
-    #print(inp)
     inp = inp.strip()
     getWeather(inp)
     wData = readJson()
     clouds = wData[0]
     humidity = wData[1]
-    
     max = wData[2]
     min = wData[3]
     location = wData[4]
     wind = wData[5]
-    update(clouds, humidity, max, min, location, wind)
-
-    
+    update(clouds, humidity, max, min, location, wind)    
     msg =   "Location : " + location + "\n\n Clouds : " + clouds + "\n Humidity : " + str(humidity) + "\n Maximum Temperature : " + str(max) + "\n Minimum Temperature : " + str(min) + "\n Wind Speed   : " + str(wind)
     #msg = "Clouds : "+ clouds +" \n Humidity : " + shum
-
-    sendmsg(msg)
-    
-    
-
-
-#getWeather(city)
-#wData = readJson()
-#msg = "Clouds " + wData[0] + "\n Humidity " + str(wData[1]) + "\n Maximum Temperature " + str(wData[2])
-#sendmsg(msg)
-#gui(wData[0], wData[1], wData[2], wData[3], wData[4])
-
-
-
 
 
 top = tkinter.Tk()
    # top.geometry("500x200")
+top.configure(bg = '#343d46')
 
-input = tkinter.Text(top, show=None, height=1, width= 40, font =('michroma', 12) )
-input.grid(row=2, column=2)
-btn = tkinter.Button(top, text = "GO", command=getInput)
-label = tkinter.Label(top, text= "Insta Weather" , font='michroma 14 bold', relief=tkinter.FLAT).grid(row=0, column=2)
-label1 = tkinter.Label(top, text= "City", font='michroma 10', relief=tkinter.FLAT).grid(row=2, column=0)  
-label1 = tkinter.Label(top, text= "Location", font='michroma 10', relief=tkinter.FLAT).grid(row=5, column=1)
-label1 = tkinter.Label(top, text= "Clouds ", font='michroma 10', relief=tkinter.FLAT).grid(row=6, column=1)
-label2 = tkinter.Label(top, text= "Humidity", font='michroma 10', relief=tkinter.FLAT).grid(row=7, column=1)
-label3 = tkinter.Label(top, text= "Max Temperature", font='michroma 10', relief=tkinter.FLAT).grid(row=8, column=1) 
-label4 = tkinter.Label(top, text= "Min Temperature", font='michroma 10', relief=tkinter.FLAT).grid(row=9, column=1)
-label5 = tkinter.Label(top, text= "Wind", font='michroma 10', relief=tkinter.FLAT).grid(row=10, column=1)
+input = tkinter.Text(top, show=None, height=1, width= 25, font =('michroma', 12) )
+input.grid(row=2, column=2, padx = 15,
+pady=15)
+btn = tkinter.Button(top, text = "GO", width=10,command=getInput)
+label = tkinter.Label(top, text= "Insta Weather" , font='michroma 25 bold italic', bg = "#343d46", fg="#eeeeee", relief=tkinter.FLAT).grid(row=0, column=2, padx=20)
+label1 = tkinter.Label(top, text= "City", font='michroma 15 bold',bg = "#343d46", fg="#eeeeee", relief=tkinter.FLAT).grid(row=2, column=1)  
+label1 = tkinter.Label(top, text= "Location", font='michroma 12',bg = "#343d46", fg="#c0c5ce", relief=tkinter.FLAT).grid(row=5, column=1)
+label1 = tkinter.Label(top, text= "Clouds ", font='michroma 12',bg = "#343d46", fg="#c0c5ce", relief=tkinter.FLAT).grid(row=6, column=1)
+label2 = tkinter.Label(top, text= "Humidity", font='michroma 12',bg = "#343d46", fg="#c0c5ce", relief=tkinter.FLAT).grid(row=7, column=1)
+label3 = tkinter.Label(top, text= "Max Temperature", font='michroma 12',bg = "#343d46", fg="#c0c5ce", relief=tkinter.FLAT).grid(row=8, column=1) 
+label4 = tkinter.Label(top, text= "Min Temperature", font='michroma 12',bg = "#343d46", fg="#c0c5ce", relief=tkinter.FLAT).grid(row=9, column=1)
+label5 = tkinter.Label(top, text= "Wind", font='michroma 12',bg = "#343d46", fg="#c0c5ce", relief=tkinter.FLAT).grid(row=10, column=1)
 
-wlabel1 =  tkinter.Label(top, show= None, font='michroma 10' , fg = "red",relief=tkinter.FLAT)
+wlabel1 =  tkinter.Label(top, show= None, font='michroma 10 bold' ,bg = '#343d46', fg = "red",relief=tkinter.FLAT)
 wlabel1.grid(row=5, column=2)
-wlabel5 =  tkinter.Label(top, show= None, font='michroma 10' , fg = "red",relief=tkinter.FLAT)
+wlabel5 =  tkinter.Label(top, show= None, font='michroma 10 bold' , bg = '#343d46',fg = "red",relief=tkinter.FLAT)
 wlabel5.grid(row=6, column=2)
-wlabel2 =  tkinter.Label(top, show= None,  font='michroma 10' , fg = "red",relief=tkinter.FLAT)
+wlabel2 =  tkinter.Label(top, show= None,  font='michroma 10 bold' , bg = '#343d46',fg = "red",relief=tkinter.FLAT)
 wlabel2.grid(row=7, column=2)
-wlabel3 =  tkinter.Label(top, show= None ,font='michroma 10' , fg = "red",relief=tkinter.FLAT)
+wlabel3 =  tkinter.Label(top, show= None ,font='michroma 10 bold' ,bg = '#343d46', fg = "red",relief=tkinter.FLAT)
 wlabel3.grid(row=8, column=2)
-wlabel4 =  tkinter.Label(top, show= None ,font='michroma 10' , fg = "red",relief=tkinter.FLAT)
+wlabel4 =  tkinter.Label(top, show= None ,font='michroma 10 bold' ,bg = '#343d46', fg = "red",relief=tkinter.FLAT)
 wlabel4.grid(row=9, column=2)
-wlabel6 =  tkinter.Label(top, show= None ,font='michroma 10' , fg = "red",relief=tkinter.FLAT)
+wlabel6 =  tkinter.Label(top, show= None ,font='michroma 10 bold' ,bg = '#343d46', fg = "red",relief=tkinter.FLAT)
 wlabel6.grid(row=10, column=2)
-btn.grid(row=11, column=2)
+btn.grid(row=11, column=2, pady = 15)
 top.mainloop()
--- webhook.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, request, Response
-app = Flask(__name__)
-@app.route('/webhook', methods=['POST'])
-def return_response():
-    print(request.json)
-    ## Do something with the request.json data.
-    return Response(status=200)
-if __name__ == "__main__": app.run()
